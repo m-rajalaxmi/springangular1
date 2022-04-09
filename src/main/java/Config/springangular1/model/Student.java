829bdb620edaacee74cd8916a4f013b4c330c668
m-rajalaxmi <39236751+m-rajalaxmi@users.noreply.github.com>
2022-04-09 07:28:37 -0400

    Update Student.java
    Add a Comment

--- src/main/java/Config/springangular1/model/Student.java
+++ src/main/java/Config/springangular1/model/Student.java
@@ -1,6 +1,6 @@
 package Config.springangular1.model;
 
-	  
+	  //Model Class
 	import javax.persistence.Entity;  
 	import javax.persistence.GeneratedValue;  
 	import javax.persistence.GenerationType;  
